6008526cb50f0fd3260f06c8d6232436d8c00709
Abdullatif YILMAZ <latifyilmaza@gmail.com>
2020-10-04 21:30:13 +0300

    dumb

---
+++ date.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Here's My Date</title>
+</head>
+<body>
+    <center>
+    <h1>Here's My Date</h1>
+    <h2><?php
+    echo $_GET['date'];
+    ?></h2>
+    </center>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Where's My Date</title>
+</head>
+<body>
+    <center>
+    <h1>Where's My Date</h1>
+    <form method="GET" action="date.php">
+    <input name="date" class="date" type="date" required>
+    <input class="submit" type="submit" value="Find" >
+    </form>
+    </center>
+</body>
+</html>
